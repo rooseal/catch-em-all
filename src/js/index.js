10162dc82574f382d368f0a6bb5074b6940cb20e
Alyssa Roose <roose.alyssa@gmail.com>
2017-12-23 14:16:59 +0100

    Add pokemons to the list

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom';
 export class CatchEmAll extends React.Component {
     render() {
         return (
-            <div className='container'>
+            <div className='app-container'>
                 <h1>Collect all pokemons</h1>
                 <h2>List of your pokemons</h2>
+                <div className='pokemon-container'>
+                    <p className="pokemon-list-entry">Chamander<span>5</span></p>
+                    <p className="pokemon-list-entry">Pidgey<span>3</span></p>
+                </div>
             </div>
         )
     }
