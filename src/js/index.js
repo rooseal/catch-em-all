238d742ff9d1ba6e97e15e4208cf6e23e0a7ee95
Alyssa Roose <roose.alyssa@gmail.com>
2017-12-26 22:50:11 +0100

    Change static template to use service data

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as pokemonService from './pokemon-service';
+
+import '../scss/main.scss';
+
 export class CatchEmAll extends React.Component {
+
+    pokeData;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            team: []
+        }
+
+        this.pokeData = pokemonService.getPokemonData();
+    }
+
+    componentDidMount() {
+        this.setState({
+            team: pokemonService.getPokemonTeam()
+        });
+    }
+
+    renderPokemon(pokemon) {
+        let basePokemon = this.pokeData[pokemon.name];
+
+        console.log(basePokemon);
+
+        return <p key={pokemon.name + pokemon.id} className={'pokemon-list-entry ' + basePokemon.type[0]}>{pokemon.name}<span>{pokemon.level}</span></p>
+    }
+
     render() {
         return (
             <div className='app-container'>
                 <h1>Collect all pokemons</h1>
                 <h2>List of your pokemons</h2>
                 <div className='pokemon-container'>
-                    <p className="pokemon-list-entry">Chamander<span>5</span></p>
-                    <p className="pokemon-list-entry">Pidgey<span>3</span></p>
+                    { 
+                        this.state.team.map(pokemon => this.renderPokemon(pokemon))
+                    }
                 </div>
+                <button>get random pokemon</button>
+                <button>release a pokemon</button>
+                <button>go to battle map</button>
             </div>
         )
     }
