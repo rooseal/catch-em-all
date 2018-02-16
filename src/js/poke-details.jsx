6f22c8f5277d436f5283b46ece3d2d203114812c
Alyssa Roose <roose.alyssa@gmail.com>
2018-02-16 22:24:35 +0100

    Edit of pokemon-details view

--- src/js/poke-details.jsx
+++ src/js/poke-details.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default props => (
-  <div>
-    { JSON.stringify(props.pokemon) }
-  </div>
-)
+export default props => {
+  const { pokemon } = props
+
+  return (
+    <div className="pokemon-details">
+      { JSON.stringify(pokemon) }
+      <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.number}.png`} />
+      <h2>{pokemon.name}</h2>
+    </div>
+  )
+}
