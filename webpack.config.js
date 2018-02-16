871c6902a2fe2bf7889e7edf19141e5a1b20daff
Alyssa Roose <roose.alyssa@gmail.com>
2018-02-16 12:59:24 +0100

    Include json-loader and eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,50 @@
 const path = require('path')
 
 module.exports = exports = env => ({
-    entry: './src/js/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/public/'
-    },
-    resolve: {
-        extensions: [ '.js', '.jsx', '.json', '.scss' ]
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.s?css$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            }
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss']
+  },
+  module: {
+    loaders: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: [
+          /node_modules/
+        ],
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
         ]
-    }
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loader: 'json-loader'
+      }
+    ]
+  }
 })
