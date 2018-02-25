85ba15521e3b8e1cf5c1df80d49f36a4b16dd499
Alyssa Roose <roose.alyssa@gmail.com>
2018-02-25 10:14:29 +0100

    Change entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = exports = env => ({
-  entry: './src/js/app.jsx',
+  entry: './src/js/components/app.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
