84056ebe05d0196bdece4b0a72947d1e5303fd56
Alyssa Roose <roose.alyssa@gmail.com>
2018-03-12 06:02:46 +0100

    Add signup form

--- src/app/js/components/auth/login.jsx
+++ src/app/js/components/auth/login.jsx
@@ -5,8 +5,11 @@ import authService from '../../services/auth'
 
 class Login extends React.Component {
   state = {
-    username: '',
-    password: '',
+    loginUsername: '',
+    loginPassword: '',
+    signupUsername: '',
+    signupPassword: '',
+    signupPassword2: '',
     loggedIn: authService.isAuth()
   }
 
@@ -18,12 +21,24 @@ class Login extends React.Component {
     })
   }
 
-  handleChangeName = e => this.setState({
-    username: e.target.value
+  handleChangeLoginName = e => this.setState({
+    loginUsername: e.target.value
   })
 
-  handleChangePassword = e => this.setState({
-    password: e.target.value
+  handleChangeLoginPassword = e => this.setState({
+    loginPassword: e.target.value
+  })
+
+  handleChangeSignupName = e => this.setState({
+    signupUsername: e.target.value
+  })
+
+  handleChangeSignupPassword = e => this.setState({
+    signupPassword: e.target.value
+  })
+
+  handleChangeSignupPassword2 = e => this.setState({
+    signupPassword2: e.target.value
   })
 
   render () {
@@ -31,10 +46,20 @@ class Login extends React.Component {
       this.state.loggedIn
         ? <Redirect to="/" />
         : (
-          <div>
-            <input type="text" placeholder="username" value={this.state.username} onChange={this.handleChangeName} />
-            <input type="password" placeholder="password" value={this.state.password} onChange={this.handleChangePassword} />
-            <button className="big-button" onClick={this.handleLogin}>login</button>
+          <div className="flex-parent" style={{justifyContent: 'space-evenly', alignItems: 'flex-end'}}>
+            <div style={{padding: '20px'}}>
+              <p className="decorative1">Already a member</p>
+              <input type="text" placeholder="email adress" value={this.state.loginUsername} onChange={this.handleChangeLoginName} />
+              <input type="password" placeholder="password" value={this.state.loginPassword} onChange={this.handleChangeLoginPassword} />
+              <button onClick={this.handleLogin}>login</button>
+            </div>
+            <div style={{padding: '20px'}}>
+              <p className="decorative1">First time visitor</p>
+              <input type="text" placeholder="email adress" value={this.state.signupUsername} onChange={this.handleChangeSignupName} />
+              <input type="password" placeholder="Choose password" value={this.state.signupPassword} onChange={this.handleChangeSignupPassword} />
+              <input type="password" placeholder="Repeat password" value={this.state.signupPassword2} onChange={this.handleChangeSignupPassword2} />
+              <button onClick={this.handleSignup}>Signup</button>
+            </div>
           </div>
         )
     )
